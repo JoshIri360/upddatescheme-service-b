d4ccfadef03424dd8250598b10fd698cb9abf336
Joshua Aideloje <91752742+JoshIri360@users.noreply.github.com>
2023-09-08 09:39:26 +0100

    tested and improved the workflow

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import fs from "fs";
+import bodyParser from "body-parser"; // Add body-parser to parse JSON requests
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json()); // Use body-parser to parse JSON requests
 
 // File paths for typeDefs and UserModel files
 const typeDefsFilePath = "./typeDefs.js";
@@ -31,6 +33,30 @@ app.get("/mongodb-config", (req, res) => {
     res.send(userModelContent);
   } catch (error) {
     console.error("Error reading UserModel file:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Handle POST request to update typeDefs file
+app.post("/update-graphql-config", (req, res) => {
+  try {
+    const newTypeDefsContent = req.body.typeDefsContent; // Assuming the request body contains the new content
+    fs.writeFileSync(typeDefsFilePath, newTypeDefsContent, "utf-8");
+    res.status(200).send("TypeDefs file updated successfully.");
+  } catch (error) {
+    console.error("Error updating typeDefs file:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Handle POST request to update UserModel file
+app.post("/update-mongodb-config", (req, res) => {
+  try {
+    const newUserModelContent = req.body.userModelContent; // Assuming the request body contains the new content
+    fs.writeFileSync(userModelFilePath, newUserModelContent, "utf-8");
+    res.status(200).send("UserModel file updated successfully.");
+  } catch (error) {
+    console.error("Error updating UserModel file:", error);
     res.status(500).send("Internal Server Error");
   }
 });
